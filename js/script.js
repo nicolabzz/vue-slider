16519a4b5a3b78c40f84eb72beca659feb51eefe
nicolabzz <nbuzzigoli@gmail.com>
2022-11-16 10:39:37 +0100

    ⚡Slider working

--- js/script.js
+++ js/script.js
@@ -11,3 +11,55 @@ Consigli del giorno:
 - regola d'oro: riciclare ovunque possibile! Questo significa che per la parte di markup possiamo recuperare html e css dell'esercizio svolto qualche giorno fa: è già tutto pronto!
 - il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe ;)
 */
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        timer: null,
+        currentSlide: 0,
+        picArray: [
+            {
+                title: 'Ed Edd n Eddy',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                items: 'img/ed-edd-n-eddy.jpg'
+            },
+            {
+                title: 'FullMetal Alchemist',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                items: 'img/fullmetal-alchemist.jpg'
+            },
+            {
+                title: 'Jocker',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                items: 'img/joker.jpg'
+            },
+            {
+                title: 'Rick n Morty',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                items: 'img/rick-e-morty.jpg'
+            },
+            {
+                title: 'South Park',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                items: 'img/south-park.png'
+            }
+        ]
+    },
+    methods: {
+        next: function () {
+            this.currentSlide < this.picArray.length - 1 ? this.currentSlide++ : this.currentSlide = 0;
+        },
+        prev: function () {
+            this.currentSlide > 0 ? this.currentSlide-- : this.currentSlide = this.picArray.length - 1;
+        },
+        startTimer: function () {
+            this.timer = setInterval(this.next, 2000);
+        },
+        stopTimer: function () {
+            clearInterval(this.timer);
+        }
+    },
+    created() {
+        this.startTimer();
+    }
+});
